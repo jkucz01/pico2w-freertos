6de6409681a33c428d5e9a10d3d728b5fa9ef374
Jacob Kuczynski <jkucz@berkeley.edu>
2025-03-28 14:16:19 -0700

    running 2 tasks at one

--- src/printf-blink.c
+++ src/printf-blink.c
@@ -1,9 +1,17 @@
 #include "FreeRTOS.h"
 #include "task.h"
+#include "timers.h"
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
 #define BLINK_DELAY_MS 500
+#define TIMER_PERIOD 1000
+
+TimerHandle_t timer_handle;
+
+void timer_callback(TimerHandle_t xTimer) {
+    printf("Hello FreeRTOS world!\n");
+}
 
 void blink_task(void *pvParameters) {
     if (cyw43_arch_init()) {
@@ -23,8 +31,13 @@ void blink_task(void *pvParameters) {
 int main() {
     stdio_init_all();
 
+    timer_handle = xTimerCreate("1 second timer", TIMER_PERIOD, pdTRUE, NULL, timer_callback);
+    xTimerStart(timer_handle, 0);
+
     xTaskCreate(blink_task, "LED Blink Task", 128, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
+    // Never reached
+    while (true) tight_loop_contents();
 }
