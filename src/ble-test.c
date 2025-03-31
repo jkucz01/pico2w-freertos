ecb86f4eaf0126264810d12d16a2605da8e3580e
Jacob Kuczynski <jkucz@berkeley.edu>
2025-03-31 10:47:51 -0700

    created bstack_config and edited cmake

--- src/ble-test.c
+++ src/ble-test.c
@@ -7,24 +7,53 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 #include "btstack.h"
-#include "btstack_event.h"
 
 static btstack_packet_callback_registration_t hci_event_callback_registration;
+
+static const uint8_t adv_data[] = {
+    2, BLUETOOTH_DATA_TYPE_FLAGS, 0x06,
+    9, BLUETOOTH_DATA_TYPE_COMPLETE_LOCAL_NAME, 'P', 'i', 'c', 'o', 'B', 'L', 'E'
+};
 
 static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
     UNUSED(size);
     UNUSED(channel);
+
     bd_addr_t local_addr;
+
     if (packet_type != HCI_EVENT_PACKET) return;
     switch(hci_event_packet_get_type(packet)){
         case BTSTACK_EVENT_STATE:
             if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING) return;
             gap_local_bd_addr(local_addr);
             printf("BTstack up and running on %s.\n", bd_addr_to_str(local_addr));
+            
+            gap_advertisements_set_params(0xA0, 0xA0, 0, 0, NULL, 0x07);
+            gap_advertisements_set_data(sizeof(adv_data), (uint8_t*) adv_data);
+            gap_advertisements_enable(1);
+            printf("Advertising as '%s'...\n", DEVICE_NAME);
+            break;
+        case HCI_EVENT_DISCONNECTION_COMPLETE:
+            printf("Disconnected...\n");
+            gap_advertisements_enable(1);
+            break;
+        case HCI_EVENT_LE_META:
+            if (hci_event_le_meta_get_subevent_code(packet) == HCI_SUBEVENT_LE_CONNECTION_COMPLETE) {
+                printf("Device connected!\n");
+                gap_advertisements_enable(0);
+            }
             break;
         default:
             break;
     }
+}
+
+void bt_task(void) {
+    int res = bt_init();
+    if (res) return;
+
+    hci_power_control(HCI_POWER_ON);
+    btstack_run_loop_execute();
 }
 
 int bt_init(void) {
@@ -34,13 +63,19 @@ int bt_init(void) {
     }
     hci_event_callback_registration.callback = &packet_handler;
     hci_add_event_handler(&hci_event_callback_registration);
+
+    l2cap_init();
+    sm_init();
+    sm_set_io_capabilities(IO_CAPABILITY_NO_INPUT_NO_OUTPUT);
 }
 
 int main() {
     stdio_init_all();
 
-    xTaskCreate(blink_task, "LED Blink Task", 128, NULL, 1, NULL);
+    xTaskCreate(blink_task, "BLE Task", 128, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
+    // Never reached
+    while (true) tight_loop_contents();
 }
