6355aecbb1567d4c4e1f13736735b448878a44b7
Jacob Kuczynski <jkucz@berkeley.edu>
2025-03-31 12:28:02 -0700

    bluetooth connection works

--- src/ble-test.c
+++ src/ble-test.c
@@ -11,10 +11,12 @@
 static btstack_packet_callback_registration_t hci_event_callback_registration;
 int bt_init(void);
 
-static const uint8_t adv_data[] = {
-    2, BLUETOOTH_DATA_TYPE_FLAGS, 0x06,
-    9, BLUETOOTH_DATA_TYPE_COMPLETE_LOCAL_NAME, 'P', 'i', 'c', 'o', 'B', 'L', 'E'
+static uint8_t adv_data[] = {
+    0x02, BLUETOOTH_DATA_TYPE_FLAGS, 0x06,
+    0x0B, BLUETOOTH_DATA_TYPE_COMPLETE_LOCAL_NAME, 'P', 'i', 'c', 'o', '-', 'B', 'L', 'E', ' ', '1',
+    0x03, BLUETOOTH_DATA_TYPE_COMPLETE_LIST_OF_16_BIT_SERVICE_CLASS_UUIDS, 0x1A, 0x18
 };
+
 
 static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
     UNUSED(size);
@@ -28,10 +30,13 @@ static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packe
             if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING) return;
             gap_local_bd_addr(local_addr);
             printf("BTstack up and running on %s.\n", bd_addr_to_str(local_addr));
-            
+            bd_addr_t null_addr;
+            memset(null_addr, 0, 6);
+            gap_advertisements_set_params(800, 800, 0, 0, null_addr, 0x07, 0x00);
+            sleep_ms(500);
             gap_advertisements_set_data(sizeof(adv_data), (uint8_t*) adv_data);
             gap_advertisements_enable(1);
-
+            printf("Advertising started...\n");
             break;
         case HCI_EVENT_DISCONNECTION_COMPLETE:
             printf("Disconnected...\n");
@@ -53,8 +58,8 @@ void bt_task(void *pvParameters) {
     if (res) return;
 
     hci_power_control(HCI_POWER_ON);
-    while (1){
-        btstack_run_loop_execute();
+    while(true) {      
+        sleep_ms(1000);
     }
 }
 
@@ -65,12 +70,16 @@ int bt_init(void) {
     }
     hci_event_callback_registration.callback = &packet_handler;
     hci_add_event_handler(&hci_event_callback_registration);
+    return 0;
 }
 
 int main() {
     stdio_init_all();
+    sleep_ms(4000);
 
-    xTaskCreate(bt_task, "BLE Task", 128, NULL, 1, NULL);
+    printf("Program started\n");
+
+    xTaskCreate(bt_task, "BLE Task", 1024, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
